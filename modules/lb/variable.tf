b9db03bd763d427cb9b9e48d1b9b0cbc42eb4925
RulerChen <a20196780@gmail.com>
2025-04-28 21:41:10 +0800

    add modules

---
+++ modules/lb/variable.tf
@@ -0,0 +1,45 @@
+variable "lb_ipv4_name" {
+  description = "The name of the load balancer IPv4 address"
+  type        = string
+}
+
+variable "gcs_backend_name" {
+  description = "The name of the GCS backend bucket"
+  type        = string
+}
+
+variable "gcs_bucket_name" {
+  description = "The name of the frontend bucket"
+  type        = string
+}
+
+variable "neg_name" {
+  description = "The name of the network endpoint group"
+  type        = string
+}
+
+variable "neg_zone" {
+  description = "The zone of the network endpoint group"
+  type        = string
+}
+
+variable "network_self_link" {
+  description = "The self link of the network"
+  type        = string
+}
+
+variable "subnet_self_link" {
+  description = "The self link of the subnet"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "The domain name for the application"
+  type        = string
+}
+
