ffd7a9d1aee14e0496dfae9dc1ed97ddd00870f2
RulerChen <a20196780@gmail.com>
2025-04-29 23:05:59 +0800

    adjust sequence

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -39,14 +39,14 @@ resource "google_container_cluster" "primary" {
     enable_private_endpoint = false
     master_ipv4_cidr_block  = "172.16.0.0/28"
   }
+  workload_identity_config {
+    workload_pool = "${var.project_id}.svc.id.goog"
+  }
 
   secret_manager_config {
     enabled = true
   }
 
-  workload_identity_config {
-    workload_pool = "${var.project_id}.svc.id.goog"
-  }
 
   deletion_protection = false
 }
